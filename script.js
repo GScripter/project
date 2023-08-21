06ca259e454587cd73672c9f6ded3ee5886b2527
Gabriel Santana <69887726+GScripter@users.noreply.github.com>
2023-08-21 17:58:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ document.getElementById("close").addEventListener("click", () => {
     document.getElementById("container-notify").style = "left: -100%;"
 })
 
+var contentLoaded = false;
 window.addEventListener("scroll", function() {
-    const scrollPercentage = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
-
-    if (scrollPercentage >= 50) {
-        document.getElementById("container-notify").style = "left: 5px;"
+    if (contentLoaded == false){
+        const scrollPercentage = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
+        if (scrollPercentage >= 50) {
+            document.getElementById("container-notify").style = "left: 5px;"
+            contentLoaded = true;
+        }
     }
 });
