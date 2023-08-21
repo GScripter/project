58983cc3e3078bac9cdadc120306c0495361b34d
gabriel <santosgabriel6763@gmail.com>
2023-08-21 13:35:37 -0300

    Add recommended.html file

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ document.getElementById("close").addEventListener("click", () => {
     document.getElementById("container-notify").style = "left: -100%;"
 })
 
-function notify(){
-    document.getElementById("container-notify").style = "left: 5px;"
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const targetDiv = document.getElementById("target-div");
+    let targetDivPosition = targetDiv.offsetTop + targetDiv.clientHeight / 2; // Target div center
 
-setTimeout(notify, 5000);
+    function loadStructureWhenDivVisible() {
+        if (window.pageYOffset + window.innerHeight >= targetDivPosition) {
+            // Place your code to load the structure when the div is reached
+        document.getElementById("container-notify").style = "left: 5px;"
+        }
+    }
+
+    // Add a listener for the scroll event
+    window.addEventListener('scroll', loadStructureWhenDivVisible);
+
+    // Call the function to check at the start in case the div is already visible
+    loadStructureWhenDivVisible();
+});
+
