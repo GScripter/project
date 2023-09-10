cb6ed75a05320312f70e29b55a8527d6ffa4530e
gabriel <santosgabriel6763@gmail.com>
2023-09-10 00:16:53 -0300

    Update popup

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const POPUPS = document.querySelectorAll('.container-notify');
+
+    if (POPUPS.length > 0) {
+        const POSITION = document.getElementById("popup-position");
+        POPUPS.forEach(function (popup) {
+            POSITION.appendChild(popup);
+        });
+        document.getElementById("container-notifies").remove()
+    }
+});
+
 // Add a click event to all close buttons
 const closeButtons = document.querySelectorAll(".close");
 closeButtons.forEach((button) => {
@@ -14,7 +26,7 @@ window.addEventListener("scroll", function() {
         const scrollPercentage =  (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
         if (scrollPercentage >= 50) {
             popupContainers.forEach((container) => {
-                container.style.left = "5px";
+                container.style.left = "0px";
             })
             contentLoaded = true;
         }
