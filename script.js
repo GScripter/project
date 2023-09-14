02fcb64df78ab624c2c6bb049c2b3c6730196320
gabriel <santosgabriel6763@gmail.com>
2023-09-14 00:14:21 -0300

    up

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+function updatePopUp() {
+    let screen = window.innerWidth;
+    let bottomOffset = 10; 
+    const popupElements = document.querySelectorAll(".container-notify:not([my-data])");
+
+    popupElements.forEach((container, index) => {
+        if (screen > 700) {
+            container.style = `left: 5px; transform: translateX(0%); bottom: ${bottomOffset}px;`;
+        } else {
+            container.style = `left: 50%; transform: translateX(-50%); bottom: ${bottomOffset}px;`;
+        }
+
+        bottomOffset += container.offsetHeight + 40; 
+    });
+}
+
 // Add a click event to all close buttons
 const closeButtons = document.querySelectorAll(".close");
 closeButtons.forEach((button) => {
     button.addEventListener("click", (evt) => {
-        evt.target.parentNode.style.left = "-100%";
+        const removedContainer = evt.target.parentNode;
+        removedContainer.setAttribute("my-data", "0");
+        removedContainer.style.left = "-100%";
+        updatePopUp();
     });
 });
 
@@ -25,10 +44,12 @@ window.addEventListener("scroll", function() {
     if (contentLoaded == false) {
         const scrollPercentage =  (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
         if (scrollPercentage >= 50) {
-            popupContainers.forEach((container) => {
-                container.style.left = "5px";
-            })
+            updatePopUp()
             contentLoaded = true;
         }
     }
 });
+
+window.addEventListener("resize", (evt) => {
+    updatePopUp()
+})
